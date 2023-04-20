038b96ae4bce7ffe527beacfdfeac4aed4566e09
naHDop-tech <tarasov198726@gmail.com>
2023-04-20 22:26:11 +0200

    feat: add init config application

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config: ConfigService = app.get(ConfigService);
+
+  await app.listen(config.get<string>('APP_PORT'));
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
