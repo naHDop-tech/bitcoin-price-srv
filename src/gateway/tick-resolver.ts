6687c0269a071bedea2dba0e52407a6ce4e55ac7
naHDop-tech <tarasov198726@gmail.com>
2023-04-21 13:31:11 +0200

    feat: add mutex

--- src/gateway/tick-resolver.ts
+++ src/gateway/tick-resolver.ts
@@ -1,6 +1,8 @@
 import { Server } from 'socket.io';
 import { BitcoinPriceService } from '@root/bitcoin-price/bitcoin-price.service';
+import { Lock } from 'async-await-mutex-lock';
 
+const lock = new Lock();
 export enum TimerEvents {
   btcTick = 'btc-tick',
   startBtcTicker = 'start-btc-ticker',
@@ -8,11 +10,7 @@ export enum TimerEvents {
 }
 
 const userTimers = {};
-// {
-//   "user001-device:001":Timeout {/* data of timer1 */},
-//   "user002-device:002":Timeout {/* data of timer2 */},
-//   "user003-device:003":Timeout {/* data of timer3 */}
-// }
+
 export function getUserDeviceRoom(userId: string, deviceId: string) {
   return userId + deviceId;
 }
@@ -35,6 +33,10 @@ export function startTimerForUserDevice(
   timerDuration: number,
 ) {
   const timer = setInterval(async function () {
+    await lock.acquire(userId + deviceId);
+    if (!lock.isAcquired(userId + deviceId)) {
+      return;
+    }
     const res = await btcPriceSrv.getBitcoinPrice();
     sendToUserDevice(
       server,
@@ -43,16 +45,13 @@ export function startTimerForUserDevice(
       TimerEvents.btcTick.toString(),
       res,
     ); // Send tick message to user device.
-
-    console.log(`user ${userId} has a timeout`);
+    lock.release(userId + deviceId);
   }, timerDuration);
 
   userTimers[userId + deviceId] = timer; // Store timer for this user device.
 }
 
 export function stopTimerForUserDevice(userId: string, deviceId: string) {
-  console.log('GET', userId, deviceId);
-  console.log('MAP', userTimers);
   clearInterval(userTimers[userId + deviceId]); // Stop the timer for this user device.
 
   delete userTimers[userId + deviceId]; // Delete the timer for this user device from the `userTimers` object.
