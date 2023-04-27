998bc8cdfe74d91ee26a96097e09f0d97f657d5f
naHDop-tech <tarasov198726@gmail.com>
2023-04-27 17:41:11 +0200

    feat: return price from db

--- src/bitcoin-price/bitcoin-price.controller.ts
+++ src/bitcoin-price/bitcoin-price.controller.ts
@@ -2,23 +2,13 @@ import { Controller, Get } from '@nestjs/common';
 
 import { BitcoinPriceService } from '@root/bitcoin-price/bitcoin-price.service';
 import { ICommissionPrice } from '@root/calculator/calculator.interface';
-import { BitcoinService } from '@root/bitcoin/bitcoin.service';
-import { BitcoinEntity } from '@root/bitcoin/bitcoin.entity';
 
 @Controller('/api/v0')
 export class BitcoinPriceController {
-  constructor(
-    private readonly bitcoinPriceService: BitcoinPriceService,
-    private readonly bitcoinService: BitcoinService,
-  ) {}
+  constructor(private readonly bitcoinPriceService: BitcoinPriceService) {}
 
   @Get('/bitcoin-price')
   getBitcoinPrice(): Promise<ICommissionPrice> {
     return this.bitcoinPriceService.getBitcoinPrice();
-  }
-
-  @Get('/price')
-  getPrice(): Promise<BitcoinEntity> {
-    return this.bitcoinService.getPriceInfo();
   }
 }
--- src/bitcoin-price/bitcoin-price.module.ts
+++ src/bitcoin-price/bitcoin-price.module.ts
@@ -1,19 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 
-import { AxiosApiClientModule } from '@root/clients/api/axios-api-client.module';
 import { BitcoinPriceService } from '@root/bitcoin-price/bitcoin-price.service';
 import { BitcoinPriceController } from '@root/bitcoin-price/bitcoin-price.controller';
 import { CommissionCalculatorModule } from '@root/calculator/commission-calculator.module';
 import { BitcoinModule } from '@root/bitcoin/bitcoin.module';
 
 @Module({
-  imports: [
-    ConfigModule,
-    AxiosApiClientModule,
-    CommissionCalculatorModule,
-    BitcoinModule,
-  ],
+  imports: [CommissionCalculatorModule, BitcoinModule],
   providers: [BitcoinPriceService],
   controllers: [BitcoinPriceController],
   exports: [BitcoinPriceService],
